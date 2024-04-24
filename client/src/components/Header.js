7115a262f44c8a898b6b5903c0e149f7ef6b5949
Soohyun <csh0sns@naver.com>
2024-04-24 01:34:44 -0400

    feat: Create Header contents

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,22 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 function Header() {
     return (
         <>
-            Header
+            <div className="nav">
+                <NavLink to="/" className="logo">
+                    logo
+                </NavLink>
+                <NavLink to="/auth/profile">Profile</NavLink>
+            </div>
+            <div className="banner">
+                <div className="title">THEME YOUR LIFE</div>
+                <div className="menu">
+                    <NavLink to="/playlists">My Playlists</NavLink>
+                    <NavLink to="/search">Search</NavLink>
+                </div>
+            </div>
         </>
     );
 }
