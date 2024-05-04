9bdf86fdba437098937c4c93bed0ba2b3c81872a
Yousung Kim <yousungkim98@gmail.com>
2024-05-04 02:02:07 -0400

    fixed logo

--- client/src/pages/PlaylistDetailPage.js
+++ client/src/pages/PlaylistDetailPage.js
@@ -1,102 +1,122 @@
 import React from "react";
-import {useState, useEffect} from "react";
-import {NavLink, useParams} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { NavLink, useParams } from "react-router-dom";
 import Banner from "../components/Banner";
-import {handleStringSize} from "../helpers/helpers";
+import { handleStringSize } from "../helpers/helpers";
 import "../styles/PlaylistDetailPage.scss";
+import defaultImage from "../assets/image/movie_default.jpg";
 
-const config = require('../config.json');
+const config = require("../config.json");
 
 function PlaylistDetailPage() {
-    const params = useParams();
-    const playlistId = params.playlistId
-    const [playlistContents, setPlaylistContents] = useState([]);
-    const [editList, setEditList] = useState([]);
-    const [deletedItems, setDeletedItems] = useState([]);
-    const [isEditing, setIsEditing] = useState(false);
-    const [maxMood, setMaxMood] = useState([]);
+  const params = useParams();
+  const playlistId = params.playlistId;
+  const [playlistContents, setPlaylistContents] = useState([]);
+  const [editList, setEditList] = useState([]);
+  const [deletedItems, setDeletedItems] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [maxMood, setMaxMood] = useState([]);
 
-    useEffect(() => {
-        fetch(`http://${config.server_host}:${config.server_port}/playlist/${playlistId}`)
-            .then(res => res.json())
-            .then(resJson => {
-                setPlaylistContents(resJson);
-                setEditList(resJson);
-            });
+  useEffect(() => {
+    fetch(
+      `http://${config.server_host}:${config.server_port}/playlist/${playlistId}`
+    )
+      .then((res) => res.json())
+      .then((resJson) => {
+        setPlaylistContents(resJson);
+        setEditList(resJson);
+      });
 
-        fetch(`http://${config.server_host}:${config.server_port}/playlist_max_mood/${playlistId}`)
-            .then(res => res.json())
-            .then(resJson => setMaxMood(resJson[0]));
-    }, [playlistId]);
+    fetch(
+      `http://${config.server_host}:${config.server_port}/playlist_max_mood/${playlistId}`
+    )
+      .then((res) => res.json())
+      .then((resJson) => setMaxMood(resJson[0]));
+  }, [playlistId]);
 
-    const toggleEditMode = () => {
-        if (isEditing) {
-            setEditList([...playlistContents]);
-            setDeletedItems([]);
+  const toggleEditMode = () => {
+    if (isEditing) {
+      setEditList([...playlistContents]);
+      setDeletedItems([]);
+    }
+    setIsEditing(!isEditing);
+  };
+
+  const handleDelete = (mediaId) => {
+    setDeletedItems((prev) => [...prev, mediaId]);
+    setEditList(editList.filter((item) => item.media_id !== mediaId));
+  };
+
+  const handleSubmit = async () => {
+    for (let mediaId of deletedItems) {
+      await fetch(
+        `http://${config.server_host}:${config.server_port}/delete_media`,
+        {
+          method: "DELETE",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ playlist_id: playlistId, media_id: mediaId }),
         }
-        setIsEditing(!isEditing);
+      );
     }
+    setPlaylistContents(editList);
+    setDeletedItems([]);
+    setIsEditing(false);
+  };
 
-    const handleDelete = mediaId => {
-        setDeletedItems(prev => [...prev, mediaId]);
-        setEditList(editList.filter(item => item.media_id !== mediaId));
-    }
-
-    const handleSubmit = async () => {
-        for (let mediaId of deletedItems) {
-            await fetch(`http://${config.server_host}:${config.server_port}/delete_media`, {
-                method: 'DELETE',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({playlist_id: playlistId, media_id: mediaId})
-            });
-        }
-        setPlaylistContents(editList);
-        setDeletedItems([]);
-        setIsEditing(false)
-    }
-
-    return (
-        <>
-            <Banner/>
-            <div className="title-wrap">
-                <div className="titles">
-                    <div className="title">{playlistContents[0] ? playlistContents[0].playlist_title : ""}</div>
-                    <div className="max-mood">{maxMood.max_mood}</div>
-                </div>
-                <div className="buttons">
-                    <button onClick={toggleEditMode}>{isEditing ? "Cancel" : "Edit"}</button>
-                    {isEditing ? (
-                        <button onClick={handleSubmit} className="submit">Submit</button>
-                    ) : (
-                        <NavLink to="/search">
-                            <div className="more">Search More Media +</div>
-                        </NavLink>
-                    )}
-                </div>
-
+  return (
+    <>
+      <Banner />
+      <div className="title-wrap">
+        <div className="titles">
+          <div className="title">
+            {playlistContents[0] ? playlistContents[0].playlist_title : ""}
+          </div>
+          <div className="max-mood">{maxMood.max_mood}</div>
+        </div>
+        <div className="buttons">
+          <button onClick={toggleEditMode}>
+            {isEditing ? "Cancel" : "Edit"}
+          </button>
+          {isEditing ? (
+            <button onClick={handleSubmit} className="submit">
+              Submit
+            </button>
+          ) : (
+            <NavLink to="/search">
+              <div className="more">Search More Media +</div>
+            </NavLink>
+          )}
+        </div>
+      </div>
+      <div className="media-list">
+        {editList.map((media) => (
+          <div className="media" key={media.media_id}>
+            <div className="top">
+              <div className="media-type">
+                {(media.media_type || " ").toUpperCase()}
+              </div>
+              {isEditing && (
+                <button onClick={() => handleDelete(media.media_id)}>X</button>
+              )}
             </div>
-            <div className="media-list">
-                {editList.map(media =>
-                    <div className="media" key={media.media_id}>
-                        <div className="top">
-                            <div className="media-type">
-                                {(media.media_type || " ").toUpperCase()}
-                            </div>
-                            {isEditing && (
-                                <button onClick={() => handleDelete(media.media_id)}>X</button>
-                            )}
-                        </div>
-                        <NavLink to={`/media/${media.media_id}`}>
-                            <img src={media.image} alt=""/>
-                        </NavLink>
-                        <NavLink to={`/media/${media.media_id}`} className="media-title">
-                            {handleStringSize(media.title)}
-                        </NavLink>
-                    </div>
-                )}
-            </div>
-        </>
-    );
+            <NavLink to={`/media/${media.media_id}`}>
+              <img
+                onError={(e) => {
+                  e.target.onerror = null;
+                  e.target.src = defaultImage;
+                }}
+                src={media.image}
+                alt="media image"
+              />
+            </NavLink>
+            <NavLink to={`/media/${media.media_id}`} className="media-title">
+              {handleStringSize(media.title)}
+            </NavLink>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default PlaylistDetailPage;
--- client/src/pages/SuggestionPage.js
+++ client/src/pages/SuggestionPage.js
@@ -1,88 +1,106 @@
-import React, {useEffect, useState} from 'react';
-import {NavLink, useNavigate} from 'react-router-dom';
-import "../styles/SuggestionPage.scss"
-import {handleStringSize} from "../helpers/helpers";
+import React, { useEffect, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import "../styles/SuggestionPage.scss";
+import { handleStringSize } from "../helpers/helpers";
 import Banner from "../components/Banner";
+import defaultImage from "../assets/image/movie_default.jpg";
 
-const config = require('../config.json');
+const config = require("../config.json");
 
 function SuggestionPage() {
-    const [resultData, setResultData] = useState([]);
-    
-    const playlistId = localStorage.getItem('playlistId');
-    console.log("Retrieved playlistId from local storage:", playlistId);
+  const [resultData, setResultData] = useState([]);
 
-    
-    const [addedMedia, setAddedMedia] = useState({});
-    const navigate = useNavigate();
+  const playlistId = localStorage.getItem("playlistId");
+  console.log("Retrieved playlistId from local storage:", playlistId);
 
-    useEffect(() => {
-        fetch(`http://${config.server_host}:${config.server_port}/suggested_media?num_media=50`)
-            .then(res => res.json())
-            .then(resJson => setResultData(resJson))
-    }, []);
+  const [addedMedia, setAddedMedia] = useState({});
+  const navigate = useNavigate();
 
-    const handleAddMedia = async (media_id) => {
-        console.log("Adding media with ID:", media_id);
-        const selectedData = {
-            playlist_id: localStorage.getItem('playlistId'),
-            media_id: media_id
-        };
-
-        const requestOptions = {
-            method: "POST", 
-            headers: {"Content-Type": "application/json"}, 
-            body: JSON.stringify(selectedData)
-        };
-        // route A: /new_media, requires playlist_id and media_id
-        try {
-            const response = await fetch(`http://${config.server_host}:${config.server_port}/new_media`, requestOptions);
-            console.log("Response status:", response.status);
-            if (!response.ok) {
-                throw new Error("Failed to add media");
-            }
-            const data = await response.json();
-            console.log("Media added successfully:", data); // Log successful addition
-
-            setAddedMedia(prev => {
-                const newMediaState = { ...prev, [media_id]: true };
-                console.log("Updated addedMedia state:", newMediaState); // Log the new state
-                return newMediaState;
-            });
-        } catch (error) {
-            console.error("Failed to add media", error);
-            alert("Failed to add media");
-        }
-
-    };
-    const handleDone = () => {
-        navigate(`/playlist/${localStorage.getItem('playlistId')}`);
-    };
-
-
-
-    return (
-        <>
-            <Banner/>
-            <div id="suggest-results">
-                {resultData.map((suggested_media) =>
-                    <div className="media" key={suggested_media.media_id}>
-                        <div className="media-type">{(suggested_media.media_type || " ").toUpperCase()}</div>
-                        <NavLink to={`/media/${suggested_media.media_id}`}>
-                            <img src={suggested_media.image} alt=""/>
-                        </NavLink>
-                        <NavLink to={`/media/${suggested_media.media_id}`} className="media-title">
-                            {handleStringSize(suggested_media.title)}
-                        </NavLink>
-                        {!addedMedia[suggested_media.media_id] && (
-                            <button onClick={() => handleAddMedia(suggested_media.media_id)}>Add</button>
-                        )}
-                    </div>
-                )}
-                <button onClick={handleDone} className="done-button">Done</button>
-            </div>
-        </>
+  useEffect(() => {
+    fetch(
+      `http://${config.server_host}:${config.server_port}/suggested_media?num_media=50`
     )
+      .then((res) => res.json())
+      .then((resJson) => setResultData(resJson));
+  }, []);
+
+  const handleAddMedia = async (media_id) => {
+    console.log("Adding media with ID:", media_id);
+    const selectedData = {
+      playlist_id: localStorage.getItem("playlistId"),
+      media_id: media_id,
+    };
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(selectedData),
+    };
+    // route A: /new_media, requires playlist_id and media_id
+    try {
+      const response = await fetch(
+        `http://${config.server_host}:${config.server_port}/new_media`,
+        requestOptions
+      );
+      console.log("Response status:", response.status);
+      if (!response.ok) {
+        throw new Error("Failed to add media");
+      }
+      const data = await response.json();
+      console.log("Media added successfully:", data); // Log successful addition
+
+      setAddedMedia((prev) => {
+        const newMediaState = { ...prev, [media_id]: true };
+        console.log("Updated addedMedia state:", newMediaState); // Log the new state
+        return newMediaState;
+      });
+    } catch (error) {
+      console.error("Failed to add media", error);
+      alert("Failed to add media");
+    }
+  };
+  const handleDone = () => {
+    navigate(`/playlist/${localStorage.getItem("playlistId")}`);
+  };
+
+  return (
+    <>
+      <Banner />
+      <div id="suggest-results">
+        {resultData.map((suggested_media) => (
+          <div className="media" key={suggested_media.media_id}>
+            <div className="media-type">
+              {(suggested_media.media_type || " ").toUpperCase()}
+            </div>
+            <NavLink to={`/media/${suggested_media.media_id}`}>
+              <img
+                onError={(e) => {
+                  e.target.onerror = null;
+                  e.target.src = defaultImage;
+                }}
+                src={suggested_media.image}
+                alt="media image"
+              />
+            </NavLink>
+            <NavLink
+              to={`/media/${suggested_media.media_id}`}
+              className="media-title"
+            >
+              {handleStringSize(suggested_media.title)}
+            </NavLink>
+            {!addedMedia[suggested_media.media_id] && (
+              <button onClick={() => handleAddMedia(suggested_media.media_id)}>
+                Add
+              </button>
+            )}
+          </div>
+        ))}
+        <button onClick={handleDone} className="done-button">
+          Done
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default SuggestionPage;
