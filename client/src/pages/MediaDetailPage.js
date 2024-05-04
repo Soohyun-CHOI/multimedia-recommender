0a0f9cf23370ba74996a21c886e76d1c7e52b1f9
Soohyun <csh0sns@naver.com>
2024-05-04 06:07:22 -0400

    feat: Create adding media to playlist MediaDetailPage

--- client/src/pages/MediaDetailPage.js
+++ client/src/pages/MediaDetailPage.js
@@ -1,159 +1,215 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, {useState, useEffect} from "react";
+import {useParams} from "react-router-dom";
 import Banner from "../components/Banner";
 import config from "../config.json";
 import "../styles/MediaDetailPage.scss";
 import defaultImage from "../assets/image/movie_default.jpg";
+import {useAuth0} from "@auth0/auth0-react";
 
 function MediaDetailPage() {
-  const params = useParams();
-  const mediaId = params.mediaId;
-  const [media, setMedia] = useState([]);
+    const params = useParams();
+    const mediaId = params.mediaId;
+    const {user, isAuthenticated} = useAuth0();
+    const [media, setMedia] = useState([]);
+    const [myPlaylists, setMyPlaylists] = useState([]);
+    const [isAdding, setIsAdding] = useState(false);
+    const [selectedPlaylistId, setSelectedPlaylistId] = useState("");
 
-  useEffect(() => {
-    fetch(
-      `http://${config.server_host}:${config.server_port}/media?media_id=${mediaId}`
-    )
-      .then((res) => res.json())
-      .then((resJson) => setMedia(resJson[0]));
-  }, [mediaId]);
+    useEffect(() => {
+        fetch(
+            `http://${config.server_host}:${config.server_port}/media?media_id=${mediaId}`
+        )
+            .then(res => res.json())
+            .then(resJson => setMedia(resJson[0]));
 
-  console.log(media);
+        if (isAuthenticated) {
+            fetch(`http://${config.server_host}:${config.server_port}/user_playlist/${user.email}`)
+                .then(res => res.json())
+                .then(resJson => setMyPlaylists(resJson));
+        }
+    }, [mediaId, user, isAuthenticated]);
 
-  return (
-    <>
-      <Banner />
-      <div id="media-detail">
-        <img
-          onError={(e) => {
-            e.target.onerror = null;
-            e.target.src = defaultImage;
-          }}
-          src={media.image}
-          alt="media image"
-        />
-        <div className="contents">
-          <div className="media-type">
-            {(media.media_type || " ").toUpperCase()}
-          </div>
-          <div className="title">{media.title}</div>
-          {media.media_type === "mv" && (
-            <>
-              <div className="info-wrap">
-                <div className="info">
-                  <span>• Release Year :</span> {media.release_date}
+    const toggleAddMode = () => {
+        setIsAdding(true);
+    };
+
+    const handleCancel = () => {
+        setIsAdding(false);
+        setSelectedPlaylistId("");
+    };
+
+    const handleSubmit = async () => {
+        if (selectedPlaylistId) {
+            await fetch(`http://${config.server_host}:${config.server_port}/new_media`, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({playlist_id: selectedPlaylistId, media_id: mediaId})
+            });
+            setIsAdding(false);
+            setSelectedPlaylistId("");
+        } else {
+            alert("Please select a playlist.");
+        }
+    }
+
+    return (
+        <>
+            <Banner/>
+            <div id="media-detail">
+                <img
+                    onError={(e) => {
+                        e.target.onerror = null;
+                        e.target.src = defaultImage;
+                    }}
+                    src={media.image}
+                    alt="media image"
+                />
+                <div className="contents">
+                    <div className="title-wrap">
+                        <div>
+                            <div className="media-type">
+                                {(media.media_type || " ").toUpperCase()}
+                            </div>
+                            <div className="title">{media.title}</div>
+                        </div>
+                        {isAuthenticated &&
+                            <>
+                                {isAdding ? (
+                                    <div className="adding">
+                                        <select value={selectedPlaylistId}
+                                                onChange={e => setSelectedPlaylistId(e.target.value)}>
+                                            <option value="">Select a playlist</option>
+                                            {myPlaylists.map(playlist => (
+                                                <option key={playlist.playlist_id}
+                                                        value={playlist.playlist_id}>{playlist.title}</option>
+                                            ))}
+                                        </select>
+                                        <div className="buttons">
+                                            <button onClick={handleCancel}>Cancel</button>
+                                            <button onClick={handleSubmit}>Add</button>
+                                        </div>
+                                    </div>
+                                ) : <button onClick={toggleAddMode}>Add to My Playlist</button>}
+                            </>
+                        }
+                    </div>
+                    {media.media_type === "mv" && (
+                        <>
+                            <div className="info-wrap">
+                                <div className="info">
+                                    <span>• Release Year :</span> {media.release_date}
+                                </div>
+                                <div className="info">
+                                    <span>• Genre :</span> {media.genres}
+                                </div>
+                                <div className="info">
+                                    <span>• Cast :</span> {media.cast}
+                                </div>
+                            </div>
+                            <div className="description">
+                                <div className="title">Overview</div>
+                                <div className="content">{media.overview}</div>
+                            </div>
+                        </>
+                    )}
+                    {media.media_type === "tv" && (
+                        <>
+                            <div className="info-wrap">
+                                <div className="info">
+                                    <span>• Release Year :</span> {media.release_year}
+                                </div>
+                                <div className="info">
+                                    <span>• Genre :</span> {media.genres}
+                                </div>
+                                <div className="info">
+                                    <span>• Rating :</span> {media.rating} / 10
+                                </div>
+                                <div className="info">
+                                    <span>• Cast :</span> {media.cast}
+                                </div>
+                            </div>
+                            <div className="description">
+                                <div className="title">Synopsis</div>
+                                <div className="content">{media.synopsis}</div>
+                            </div>
+                        </>
+                    )}
+                    {media.media_type === "mu" && (
+                        <>
+                            <div className="info-wrap">
+                                <div className="info">
+                                    <span>• Artist :</span> {media.artist}
+                                </div>
+                                <div className="info">
+                                    <span>• Release Year :</span> {media.year}
+                                </div>
+                                <div className="info">
+                                    <span>• Genre :</span> {media.tag}
+                                </div>
+                                <div className="info">
+                                    <span>• Views :</span> {media.views}
+                                </div>
+                            </div>
+                            <div className="description">
+                                <div className="title">Lyrics</div>
+                                <div className="content">{media.lyrics}</div>
+                            </div>
+                        </>
+                    )}
+                    {media.media_type === "gm" && (
+                        <>
+                            <div className="info-wrap">
+                                <div className="info">
+                                    <span>• Developer :</span> {media.developers}
+                                </div>
+                                <div className="info">
+                                    <span>• Release Date :</span> {media.release_date}
+                                </div>
+                                <div className="info">
+                                    <span>• Genre :</span> {media.genres}
+                                </div>
+                                <div className="info">
+                                    <span>• Category :</span> {media.categories}
+                                </div>
+                                <div className="info">
+                                    <span>• Score :</span> {media.metacritic_score} / 100
+                                </div>
+                                <div className="info">
+                                    <span>• Price :</span> ${media.price}
+                                </div>
+                            </div>
+                            <div className="description">
+                                <div className="title">Description</div>
+                                <div className="content">{media.about_the_game}</div>
+                            </div>
+                        </>
+                    )}
+                    {media.media_type === "bk" && (
+                        <>
+                            <div className="info-wrap">
+                                <div className="info">
+                                    <span>• Author :</span> {media.authors}
+                                </div>
+                                <div className="info">
+                                    <span>• Publisher :</span> {media.publisher}
+                                </div>
+                                <div className="info">
+                                    <span>• Published Date :</span> {media.published_date}
+                                </div>
+                                <div className="info">
+                                    <span>• Genre :</span> {media.categories}
+                                </div>
+                            </div>
+                            <div className="description">
+                                <div className="title">Description</div>
+                                <div className="content">{media.description}</div>
+                            </div>
+                        </>
+                    )}
                 </div>
-                <div className="info">
-                  <span>• Genre :</span> {media.genres}
-                </div>
-                <div className="info">
-                  <span>• Cast :</span> {media.cast}
-                </div>
-                <div className="description">
-                  <div className="title">Overview</div>
-                  <div className="content">{media.overview}</div>
-                </div>
-              </div>
-            </>
-          )}
-          {media.media_type === "tv" && (
-            <>
-              <div className="info-wrap">
-                <div className="info">
-                  <span>• Release Year :</span> {media.release_year}
-                </div>
-                <div className="info">
-                  <span>• Genre :</span> {media.genres}
-                </div>
-                <div className="info">
-                  <span>• Rating :</span> {media.rating} / 10
-                </div>
-                <div className="info">
-                  <span>• Cast :</span> {media.cast}
-                </div>
-              </div>
-              <div className="description">
-                <div className="title">Synopsis</div>
-                <div className="content">{media.synopsis}</div>
-              </div>
-            </>
-          )}
-          {media.media_type === "mu" && (
-            <>
-              <div className="info-wrap">
-                <div className="info">
-                  <span>• Artist :</span> {media.artist}
-                </div>
-                <div className="info">
-                  <span>• Release Year :</span> {media.year}
-                </div>
-                <div className="info">
-                  <span>• Genre :</span> {media.tag}
-                </div>
-                <div className="info">
-                  <span>• Views :</span> {media.views}
-                </div>
-              </div>
-              <div className="description">
-                <div className="title">Lyrics</div>
-                <div className="content">{media.lyrics}</div>
-              </div>
-            </>
-          )}
-          {media.media_type === "gm" && (
-            <>
-              <div className="info-wrap">
-                <div className="info">
-                  <span>• Developer :</span> {media.developers}
-                </div>
-                <div className="info">
-                  <span>• Release Date :</span> {media.release_date}
-                </div>
-                <div className="info">
-                  <span>• Genre :</span> {media.genres}
-                </div>
-                <div className="info">
-                  <span>• Category :</span> {media.categories}
-                </div>
-                <div className="info">
-                  <span>• Score :</span> {media.metacritic_score} / 100
-                </div>
-                <div className="info">
-                  <span>• Price :</span> ${media.price}
-                </div>
-              </div>
-              <div className="description">
-                <div className="title">Description</div>
-                <div className="content">{media.about_the_game}</div>
-              </div>
-            </>
-          )}
-          {media.media_type === "bk" && (
-            <>
-              <div className="info-wrap">
-                <div className="info">
-                  <span>• Author :</span> {media.authors}
-                </div>
-                <div className="info">
-                  <span>• Publisher :</span> {media.publisher}
-                </div>
-                <div className="info">
-                  <span>• Published Date :</span> {media.published_date}
-                </div>
-                <div className="info">
-                  <span>• Genre :</span> {media.categories}
-                </div>
-              </div>
-              <div className="description">
-                <div className="title">Description</div>
-                <div className="content">{media.description}</div>
-              </div>
-            </>
-          )}
-        </div>
-      </div>
-    </>
-  );
+            </div>
+        </>
+    );
 }
 
 export default MediaDetailPage;
--- client/src/pages/PlaylistListPage.js
+++ client/src/pages/PlaylistListPage.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import "../styles/PlaylistListPage.scss"
-import { useAuth0 } from "@auth0/auth0-react";
-import { NavLink } from "react-router-dom";
+import {useAuth0} from "@auth0/auth0-react";
+import {NavLink} from "react-router-dom";
 
 
 const config = require('../config.json');
@@ -10,33 +10,33 @@ const config = require('../config.json');
 
 function PlaylistListPage() {
     const [playlists, setPlaylists] = useState([]);
-    const { user, isAuthenticated, isLoading } = useAuth0();
-    
+    const {user, isAuthenticated, isLoading} = useAuth0();
 
     useEffect(() => {
         fetch(`http://${config.server_host}:${config.server_port}/user_playlist/${user.email}`)
             .then(res => res.json())
             .then(resJson => setPlaylists(resJson));
     }, []);
+
     return (
-        <> 
+        <>
             <div className="plist">
                 <div className="title">MY PLAYLISTS</div>
                 <div className="media-wrap">
-                    {playlists.map((user_playlist)=>(
+                    {playlists.map((user_playlist) => (
                         <NavLink
                             to={`/playlist/${user_playlist.playlist_id}`} // Assuming your playlist detail route is like '/playlist/:playlist_id'
                             key={user_playlist.playlist_id}
                         >
                             <div className="item">{user_playlist.title}</div>
-                    
+
                         </NavLink>
                     ))}
                 </div>
-                
+
             </div>
-            
-            
+
+
         </>
     );
 }
