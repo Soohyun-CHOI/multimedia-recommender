c0cd88a37d79e0fefed20ddadbfd86ef26ce9925
Soohyun <csh0sns@naver.com>
2024-05-03 21:35:59 -0400

    feat: Create PlaylistDetailPage

--- client/src/pages/PlaylistDetailPage.js
+++ client/src/pages/PlaylistDetailPage.js
@@ -1,37 +1,45 @@
 import React from "react";
-import { useState, useEffect } from "react";
-
-import { Navlink } from "react-router-dom";
+import {useState, useEffect} from "react";
+import {NavLink, useParams} from "react-router-dom";
+import Banner from "../components/Banner";
+import {handleStringSize} from "../helpers/helpers";
 
 const config = require('../config.json');
 
-function PlaylistDetailPage(){
-    const [playlistId, setPlaylistId] = useState('');
+function PlaylistDetailPage() {
+    const params = useParams();
+    const playlistId = params.playlistId
     const [playlistContents, setPlaylistContents] = useState([]);
-    
-    useEffect(() => {
-        // fetch(`http://${config.server_host}:${config.server_port}/playlists`)
-        //     .then(res => res.json())
-        //     .then(resJson => {
-        //         setPlaylistId(resJson.playlistId);
-        //     })
 
+    useEffect(() => {
         fetch(`http://${config.server_host}:${config.server_port}/playlist/${playlistId}`)
             .then(res => res.json())
             .then(resJson => setPlaylistContents(resJson));
-    }, [playlistId]);
+    }, []);
+
+    console.log(playlistContents);
+
     return (
         <>
+            <Banner/>
+            <div className="title">{}</div>
             <div className="media-list">
-                {playlistContents.map((playlist)=>
-                    <div className="title" key={playlist.id}>{playlist.title}</div>
-                
+                {playlistContents.map((media) =>
+                    <div className="media" key={media.media_id}>
+                        <div className="media-type">
+                            {(media.media_type || " ").toUpperCase()}
+                        </div>
+                        <NavLink to={`/media/${media.media_id}`}>
+                            <img src={media.image} alt=""/>
+                        </NavLink>
+                        <NavLink to={`/media/${media.media_id}`} className="media-title">
+                            {handleStringSize(media.title)}
+                        </NavLink>
+                    </div>
                 )}
-
             </div>
         </>
     );
-
 }
 
 export default PlaylistDetailPage;
